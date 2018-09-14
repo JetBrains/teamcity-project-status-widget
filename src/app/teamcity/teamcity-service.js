eddcb5a070d5b8d47bc842fd243bddd932d1b0c2
Maxim Mazin <maxim.mazin@jetbrains.com>
2018-09-14 13:03:44 +0200

    Added TeamCity methods

--- src/app/teamcity/teamcity-service.js
+++ src/app/teamcity/teamcity-service.js
@@ -8,6 +8,40 @@ export default class TeamcityService {
     this.dashboardApi = dashboardApi;
   }
 
+  async getProjects(teamcityService) {
+    return await this._fetchTeamcity(teamcityService, 'projects', {
+      fields: this.recursiveConvertHash({
+        project: ['id', 'name', 'parentProjectId', 'archived']
+      })
+    });
+  }
+
+  async getBuildTypes(teamcityService) {
+    return await this._fetchTeamcity(teamcityService, 'buildTypes', {
+      fields: this.recursiveConvertHash({
+        buildType: ['id', 'name', 'projectId']
+      })
+    });
+  }
+
+  async getBuildTypesOfProject(teamcityService, projectId) {
+    return await this._fetchTeamcity(teamcityService, 'buildTypes', {
+      locator: this.recursiveConvertHash({
+        affectedProject: {id: projectId}
+      }),
+      fields: this.recursiveConvertHash({
+        buildType: ['id', 'name', 'projectId']
+      })
+    });
+  }
+
+  async getBuildType(teamcityService) {
+    return await this._fetchTeamcity(teamcityService, 'builds/:locator');
+  }
+
+  /**
+   * @deprecated
+   */
   async getInvestigations(teamcityService, locator) {
     const teamcityResponse = await this.dashboardApi.fetch(
       teamcityService.id,
@@ -24,6 +58,9 @@ export default class TeamcityService {
     return convertTeamcityResponse(teamcityService, teamcityResponse);
   }
 
+  /**
+   * @deprecated
+   */
   async getMyInvestigations(teamcityService) {
     return await this.getInvestigations(teamcityService, {
       state: 'TAKEN',
@@ -31,25 +68,37 @@ export default class TeamcityService {
     });
   }
 
+  /**
+   * @deprecated
+   */
   async getProjectInvestigations(teamcityService, projectId) {
     return await this.getInvestigations(teamcityService, {
       affectedProject: {id: projectId}
     });
   }
 
-  recursiveConvertHash = input => Object.
-    entries(input).
-    map(([key, val]) => {
-      if (!val) {
-        return null;
-      } else if (typeof val === 'object') {
-        return `${key}(${this.recursiveConvertHash(val)})`;
-      } else if (Array.isArray(val)) {
-        return `${key}(${val.map(this.recursiveConvertHash).join(',')})`;
-      } else {
-        return `${key}:${val}`;
-      }
-    }).
-    filter(it => it).
-    join(',')
+  async _fetchTeamcity(teamcityService, path, query) {
+    return await this.dashboardApi.fetch(
+      teamcityService.id,
+      `app/rest/${API_VER}/${path}`,
+      {
+        query,
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json'
+        }
+      });
+  }
+
+  recursiveConvertHash = input => Object.entries(input).map(([key, val]) => {
+    if (!val) {
+      return null;
+    } else if (typeof val === 'object') {
+      return `${key}(${this.recursiveConvertHash(val)})`;
+    } else if (Array.isArray(val)) {
+      return `${key}(${val.map(this.recursiveConvertHash).join(',')})`;
+    } else {
+      return `${key}:${val}`;
+    }
+  }).filter(it => it).join(',')
 }
