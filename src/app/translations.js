49fbd7b9a53681a62d003d29aafc59c0287b1c6d
katriyna <katriyna@gmail.com>
2018-09-14 19:14:08 +0300

    correct names for translations files

--- src/app/translations.js
+++ src/app/translations.js
@@ -1,7 +1,13 @@
-import {de} from './translations/locale_de.po';
-import {es} from './translations/locale_es.po';
-import {fr} from './translations/locale_fr.po';
-import {ru} from './translations/locale_ru.po';
-import {ja} from './translations/locale_ja.po';
+import de from './translations/teamcity-project-status-widget_de.po';
+import es from './translations/teamcity-project-status-widget_es.po';
+import fr from './translations/teamcity-project-status-widget_fr.po';
+import ru from './translations/teamcity-project-status-widget_ru.po';
+import ja from './translations/teamcity-project-status-widget_ja.po';
 
-export default {de, es, fr, ru, ja};
+export default {
+  de: de.de_DE,
+  es: es.es_ES,
+  fr: fr.fr_FR,
+  ru: ru.ru_RU,
+  ja: ja.ja_JP
+};
