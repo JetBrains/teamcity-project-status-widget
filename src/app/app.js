4f2f8dc18cb52927e60c33f06cc6aa018e2356b4
katriyna <katriyna@gmail.com>
2019-01-10 18:45:45 +0300

    SDP-361 updated dependencies

--- src/app/app.js
+++ src/app/app.js
@@ -5,7 +5,6 @@ import React from 'react';
 import {Provider} from 'react-redux';
 import {render} from 'react-dom';
 
-import 'file-loader?name=[name].[ext]!../../manifest.json'; // eslint-disable-line import/no-unresolved
 import createStore from './redux/index';
 import {initWidget} from './redux/actions';
 import WidgetContainer from './container/widget-container';
